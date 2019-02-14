121b8baa331eb94091cdbc7bad758ece74b0c2c2
Kildare Lauser <kildarealauser@gmail.com>
2019-02-14 16:31:59 -0400

    Add isUserLogged function

--- POSServices/Models/Tools.cs
+++ POSServices/Models/Tools.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -24,6 +25,47 @@ namespace POSServices.Models
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static bool isUserLogged(string token, string IdCompany)
+        {
+
+            Connection connection = new Connection();
+
+            if (connection.OpenConnection())
+            {
+
+                SqlCommand cmd = new SqlCommand("", connection.connection);
+                cmd.CommandText = "SELECT IdUser FROM UserPaseo WHERE Token = @Token";
+                cmd.Parameters.AddWithValue("@Token", token);
+                SqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (!dataReader.HasRows)
+                {
+                    return false;
+                }
+                else
+                {
+                    while (dataReader.Read())
+                    {
+                        String IdUser = dataReader["IdUser"].ToString();
+                        dataReader.Close();
+
+                        cmd.CommandText = "SELECT * FROM UserCompany WHERE IdCompany = @IdCompany AND IdUser = @IdUser";
+                        cmd.Parameters.AddWithValue("@IdCompany", IdCompany);
+                        cmd.Parameters.AddWithValue("@IdUser", IdUser);
+
+                        dataReader = cmd.ExecuteReader();
+
+                        if (dataReader.HasRows)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            
+            return false;
         }
     }
 }
